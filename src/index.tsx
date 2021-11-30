a08a4688ded817cab4101daf3f9a09a945fe954c
Paul Millet <warstrolo.wizard@gmail.com>
2021-11-30 16:59:56 +0100

    added ga4

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./i18n";
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
 
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import LblocFooter from "../lbloc/footer/footer"
 import LblocHeader from "../lbloc/header/header"
 import LblocNavbar from "../lbloc/navbar/navbar"
-import ReactGA from "react-ga"
+import ReactGA from "react-ga4"
 
 
 function PageIndex() {
-  ReactGA.pageview('/');
+  ReactGA.send({ hitType: "pageview", page: "/" });
   return (
     <>
       <LblocNavbar/>
