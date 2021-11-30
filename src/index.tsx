3e14723690d5564b593ae1aa9c33790c924cda7a
Paul Millet <warstrolo.wizard@gmail.com>
2021-11-30 12:28:02 +0100

    added ga

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./i18n";
+import ReactGA from 'react-ga';
+
+
+
+ReactGA.initialize('G-81YLF1CGXF');
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import LblocFooter from "../lbloc/footer/footer"
 import LblocHeader from "../lbloc/header/header"
 import LblocNavbar from "../lbloc/navbar/navbar"
-
+import ReactGA from "react-ga"
 
 
 function PageIndex() {
+  ReactGA.pageview('/');
   return (
     <>
       <LblocNavbar/>
